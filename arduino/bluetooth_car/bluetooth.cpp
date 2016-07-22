ec886fa97cfd722493f6a57547de41c41507ceaa
Vladislav Korecký <vkorecky@acerv7.korecky.org>
2016-07-22 09:30:45 +0200

    Refactoring

---
+++ arduino/bluetooth_car/bluetooth.cpp
@@ -0,0 +1,35 @@
+/*
+  Bluetooth.cpp - Library for bluetooth communication.
+  Created by Vladislav Korecky, March 24, 2016.
+*/
+
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include "bluetooth.h"
+
+Bluetooth::Bluetooth(int rx, int tx) : serialBT(SoftwareSerial(rx, tx))
+{
+  _rx = rx;
+  _tx = tx;
+}
+
+void Bluetooth::setup() {
+  // initialization of serial port 9600 baud
+  serialBT.begin(9600);
+  // waits on initialization
+  delay(500);
+}
+
+void Bluetooth::send(String message)
+{
+  serialBT.println(message);
+}
+
+char Bluetooth::read() {
+  char charBT;
+  if ( serialBT.available())
+  {
+    charBT = serialBT.read();
+  }
+  return charBT;
+}
---
+++ arduino/bluetooth_car/bluetooth_car.ino
@@ -0,0 +1,67 @@
+// **************************************************
+// Bluetooth car
+// -------------
+// Car comunicate with computer throught bluetooth.
+// All inteligence must be in computer software,
+// car is stupid and only receive commands and sends
+// information about enviroment to the computer
+// **************************************************
+
+// Includes
+// ---------
+#include <Servo.h>
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+#include "motors.h"
+#include "ultrasonic.h"
+#include "bluetooth.h"
+
+Bluetooth bluetooth =  Bluetooth(12, 13); //RX = 12, TX = 13
+//Ultrasonic ultrasonic =  Ultrasonic(3, 4, 5); //servo = 3; echo = 4; trig = 5;
+//Motors motors =  Motors(6, 7, 8, 9, 10, 11); // ena = 6, in1 = 7, in2 = 8, in3 = 9, in4 = 10, enb = 11
+
+//int _servoAngel = 90;
+//int _servoAngelStep = 45;
+///int _servoDirection = 1;
+
+
+// Setup
+// -----
+void setup() {
+  //Serial.begin(9600);      // open the serial port at 9600 bps:
+  bluetooth.setup();
+//  ultrasonic.setup();
+//  motors.setup();
+}
+
+// Main
+// -----
+void loop() {
+  bluetooth.send("Ahoj");
+  delay(1000);
+  
+//  long distance = ultrasonic.measure(_servoAngel); 
+//  String message = String(_servoAngel) + " : " + String(distance);  
+//  //Serial.println(message);
+//  bluetooth.send(message);
+//
+//  if ((_servoDirection == 1) && ((_servoAngel + _servoAngelStep) > 180)) {
+//    _servoDirection = 0;
+//  } else if ((_servoDirection == 0) && ((_servoAngel - _servoAngelStep) < 0)) {
+//    _servoDirection = 1;
+//  }
+//
+//  if (_servoDirection == 1) {
+//    _servoAngel = _servoAngel + _servoAngelStep;
+//  } else {
+//    _servoAngel = _servoAngel - _servoAngelStep;
+//  }
+//
+//
+//  motors.forward(255,1000);
+//  motors.backward(255,1000);
+//  motors.right(255,1000);
+//  motors.stop(1000);
+}
+
